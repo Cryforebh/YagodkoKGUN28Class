af9bf72bf4110920a8b3603de161f1f0345110cf
Cryfore (Kirill) <blackhawksk@yandex.ru>
2024-09-04 15:15:01 +0300

    memory 0.1

--- Class/Program.cs
+++ Class/Program.cs
@@ -10,6 +10,95 @@ namespace Class
     {
         static void Main(string[] args)
         {
+            Console.WriteLine($"Подготовка к бою!");
+
+            //...............................................................................
+
+            Console.Write($"\nВведите имя Первого бойца : ");
+            string nameOne = Console.ReadLine();
+
+            Templates.LimitParametersInput("Введите начальное здоровье Первого бойца (10-100): ", 100f, 10f, out float healthOne);
+
+            var unitOne = new Unit(healthOne, nameOne);
+
+            //...............................................................................
+
+            Console.Write($"\nВведите имя Второго бойца : ");
+            string nameTwo = Console.ReadLine();
+
+            Templates.LimitParametersInput("Введите начальное здоровье Второго бойца (10-100): ", 100f, 10f, out float healthTwo);
+
+            var unitTwo = new Unit(healthTwo, nameTwo);
+
+            //...............................................................................
+
+            Templates.LimitParametersInput("\nВведите значение брони шлема от 0, до 1: ", 1f, 0f, out float armorHelm);
+
+            Templates.LimitParametersInput("\nВведите значение брони кирасы от 0, до 1: ", 1f, 0f, out float armorShell);
+
+            Templates.LimitParametersInput("\nВведите значение брони сапог от 0, до 1: ", 1f, 0f, out float armorBoots);
+
+            var helm = new Helm(armorHelm);
+            var shell = new Shell(armorShell);
+            var boots = new Boots(armorBoots);
+
+            Templates.LimitParametersInput("\nУкажите минимальный урон оружия (0-20): ", 20f, 0f, out float minDamage);
+
+            Templates.LimitParametersInput("\nУкажите максимальный урон оружия (20-40): ", 40f, 20f, out float maxDamage);
+
+            var weapons = new Weapon(minDamage, maxDamage);
+
+            //...............................................................................
+
+            Console.WriteLine("\nБойцы снаряжаются...");
+
+            //...............................................................................
+
+            unitOne.EquipHelm(helm);
+            unitOne.EquipShell(shell);
+            unitOne.EquipBoots(boots);
+            unitOne.EquipWeapon(weapons);
+
+            //Console.WriteLine($"\nОбщий показатель брони равен: {unitOne.Armor}");
+            //Console.WriteLine($"Фактическое значение здоровья равно: {unitOne.RealHealth()}");
+            //Console.WriteLine($"Средний урон из {weapons.Name}: {weapons.GetDamage()}");
+            //Console.WriteLine($"Экипировано: {weapons.Name}, {helm.Name}, {shell.Name}, {boots.Name}.\n");
+
+            //...............................................................................
+
+            unitTwo.EquipHelm(helm);
+            unitTwo.EquipShell(shell);
+            unitTwo.EquipBoots(boots);
+            unitTwo.EquipWeapon(weapons);
+
+            //Console.WriteLine($"\nОбщий показатель брони равен: {unitTwo.Armor}");
+            //Console.WriteLine($"Фактическое значение здоровья равно: {unitTwo.RealHealth()}");
+            //Console.WriteLine($"Средний урон из {weapons.Name}: {weapons.GetDamage()}");
+            //Console.WriteLine($"Экипировано: {weapons.Name}, {helm.Name}, {shell.Name}, {boots.Name}.");
+
+            //...............................................................................
+
+            var combat = new Combat();
+
+            //...............................................................................
+
+            Console.WriteLine("\nБой начинается!\n");
+
+            combat.StartCombat(unitOne, unitTwo);
+            combat.ShowResults();
+
+            Console.WriteLine("\nБой завершен!");
+
+            if (unitTwo.Health <= 0)
+            {
+                Console.WriteLine($"\n{unitOne.Name} становится победителем!");
+            }
+            else if (unitOne.Health <= 0)
+            {
+                Console.WriteLine($"\n{unitTwo.Name} становится победителем!");
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Class/Unit.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class Unit
+    {
+        private float _health;
+        private float _baseDamage = 5f;
+        private Weapon _weapon;
+        private Helm _helm;
+        private Shell _shell;
+        private Boots _boots;
+
+        public string Name { get; }
+
+        public float Health => _health;
+
+        public float Damage
+        {
+            get
+            {
+                if (_weapon != null)
+                {
+                    return _baseDamage + _weapon.GetDamageInterval(); // возвращаем сумму базового урона и урона от оружия если оно есть
+                }
+                else { return _baseDamage; }
+            }
+        }
+
+        public float Armor
+        {
+            get
+            {
+                float value = 0;
+
+                if (_helm != null)
+                {
+                    value += _helm.Armor;
+                }
+
+                if (_shell != null)
+                {
+                    value += _shell.Armor;
+                }
+
+                if (_boots != null)
+                {
+                    value += _boots.Armor;
+                }
+
+
+                if (value > 1f)
+                {
+                    return 1f;
+                }
+                else if (value <= 0f)
+                {
+                    return 0f;
+                }
+                else
+                {
+                    value = (float)Math.Round(value, 2);
+                    return value;
+                }
+
+            }
+        }
+
+        public Unit(string name)
+        {
+            Name = name;
+        }
+
+        public Unit(float health, string name = "Unknown Unit") : this(name)
+        {
+            _health = health;
+        }
+
+        public Unit() : this("Unknown Unit")
+        {
+        }
+
+        public float RealHealth() { return Health * (1f + Armor); }
+
+        public bool SetDamage(float damage)
+        {
+            _health = Health - damage / (1f + Armor);
+
+            if (Health <= 0f)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+        public void EquipWeapon(Weapon weapon) // Снарядить Weapon | Замена оружия на новое
+        {
+            _weapon = weapon;
+            Console.WriteLine($"{_weapon.Name} - экипировано!");
+        }
+
+        public void EquipHelm(Helm helm) // Снарядить Helm
+        {
+            _helm = helm;
+            Console.WriteLine($"{_helm.Name} - экипировано!");
+        }
+
+        public void EquipShell(Shell shell) // Снарядить Shell
+        {
+            _shell = shell;
+            Console.WriteLine($"{_shell.Name} - экипировано!");
+        }
+
+        public void EquipBoots(Boots boots) // Снарядить Boots
+        {
+            _boots = boots;
+            Console.WriteLine($"{_boots.Name} - экипировано!");
+        }
+
+        public void DamageSkip()
+        {
+            var damageSkip = Damage - (Damage / (1f + Armor));
+            Console.WriteLine($"Поглащенно урона - {damageSkip}.\n");
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"{Name}, {_health}, {Armor}, {RealHealth()}");
+        }
+    }
+}
